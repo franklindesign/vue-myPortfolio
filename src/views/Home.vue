81e39f31ea514f686cc52fe102189fb1a108d7a5
franklindesign <manghi@gmail.com>
2019-01-31 18:20:21 -0800

    update packages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <section>
       <p class="about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum vitae earum doloremque quis, esse voluptatum ab illo ipsa pariatur quidem, ex libero in natus error est tempora officia voluptatem neque?
       </p>
+      
     </section>
   </div>
 </template>
